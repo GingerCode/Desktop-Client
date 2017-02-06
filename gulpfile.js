ebbe28338031b50b9817eda3b954cbb9c971eb56
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2017-02-06 02:25:33 +0100

    dist and build tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,14 @@ const gulp = require('gulp'),
     debug = require('gulp-debug'),
     exec = require('child_process').exec;
 
+gulp.task("dist", function(){
+    return gulp.src("build/**.zip")
+        .pipe(debug({title: 'dist (Scope):'}))
+        .pipe(gulp.dest('dist'));
+})
+
 gulp.task('zip', function (cb) {
-    exec("find . -type f -name \"*.zip\" -delete && for i in dist/*; do zip -r \"${i%/}.zip\" \"$i\"; done", function (err, stdout, stderr) {
+    exec("find ./build -type f -name \"*.zip\" -delete && for i in build/*; do zip -r \"${i%/}.zip\" \"$i\"; done", function (err, stdout, stderr) {
         // Long log better to avoid if not needed:
         // if(stdout) console.log("[ZIP TASK stdout]:", stdout);
         if(stderr) console.log("[ZIP TASK stderr]:", stderr);
